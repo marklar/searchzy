0c25ebb433033752f60e8502baee748513bc48f3
Mark Wong-VanHaren <markwvh@gmail.com>
2013-10-27 00:01:38 +0200

    Util fns for facilitating JSON responses.

--- src/searchzy/handler.clj
+++ src/searchzy/handler.clj
@@ -4,16 +4,23 @@
             [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(def json-headers
+  {"Content-Type" "application/json; charset=utf-8"})
+
+(defn ok-json-response
+  [obj]
+  {:status 200
+   :headers json-headers
+   :body (json/write-str obj)})
+  
+
 ;; COMPOJURE ROUTES
 (defroutes app-routes
 
   (GET "/" [query lat lng & others]
-       {:status 200
-        :headers {"Content-Type" "application/json; charset=utf-8"}
-        :body (json/write-str {:query query
-                               :lat lat, :lng lng
-                               :others others})
-        })
+       (ok-json-response {:query query
+                          :lat lat, :lng lng
+                          :others others}))
 
   (route/resources "/")
   (route/not-found "Not Found"))
