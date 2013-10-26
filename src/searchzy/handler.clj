0497951031372df22d57a08060e6504b87a9d62f
Mark Wong-VanHaren <markwvh@gmail.com>
2013-10-26 23:56:20 +0200

    Cleaned up handler.clj.

--- src/searchzy/handler.clj
+++ src/searchzy/handler.clj
@@ -1,19 +1,15 @@
 (ns searchzy.handler
   (:use compojure.core)
-  (:require ;; [somnium.congomongo :as mg]
-            [clojure.data.json :as json]
+  (:require [clojure.data.json :as json]
             [compojure.handler :as handler]
             [compojure.route :as route]))
 
 ;; COMPOJURE ROUTES
 (defroutes app-routes
 
-  (GET "/" [] "Hello, World!")
-
-  (GET "/businesses" [query lat lng & others]
+  (GET "/" [query lat lng & others]
        {:status 200
         :headers {"Content-Type" "application/json; charset=utf-8"}
-        ;; :body (mg/fetch-one :businesses :as :json)
         :body (json/write-str {:query query
                                :lat lat, :lng lng
                                :others others})
