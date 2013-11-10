b574a6446ddaac23f0141670874fa01f4e075cd4
Mark Wong-VanHaren <markwvh@gmail.com>
2013-11-10 05:10:51 +0100

    properly calculating aggregate info now.

--- src/searchzy/service/business_menu_items.clj
+++ src/searchzy/service/business_menu_items.clj
@@ -11,7 +11,7 @@
             [clojurewerkz.elastisch.native
              [document :as es-doc]]))
 
-(defn -mk-one-hit
+(defn- mk-one-hit
   "Replace :hours with :hours_today, using :day_of_week.
    Add :distance_in_mi."
   [source-map day-of-week coords]
@@ -33,24 +33,12 @@
         (dissoc source-map :latitude_longitude :yelp_star_rating :yelp_review_count)
       :business new-biz)))
 
-;; TODO
-(defn -prices-for-hits
-  [resp-hits]
-  {:mean 0.0
-   :minimum 0.0
-   :maximum 0.0})
-
-;; TODO
-(defn -latest-close
-  [resp-hits]
-  {:hour 0 :minute 0})
-
-(defn -mk-response
+(defn- mk-response
   "From ES response, create service response."
-  [{hits :hits} item-id geo-map page-map]
+  [{hits :hits} prices close item-id geo-map page-map]
   (let [day-of-week (util/get-day-of-week)
-        resp-hits (map #(-mk-one-hit (:_source %) day-of-week (:coords geo-map))
-                       (:hits hits))]
+        resp-hits (map #(mk-one-hit % day-of-week (:coords geo-map))
+                       (map :_source (:hits hits)))]
     (responses/ok-json
      {:endpoint "/v1/business_menu_items"   ; TODO: pass this in
       :arguments {:item_id item-id
@@ -58,18 +46,18 @@
                   :paging page-map
                   :day_of_week day-of-week}
       :results {:count (:total hits)
-                :prices_picos (-prices-for-hits resp-hits)
-                :latest_close (-latest-close resp-hits)
+                :prices_picos prices
+                :latest_close close
                 :hits resp-hits
                 }})))
 
-(def -idx-name (:index (:business_menu_items cfg/elastic-search-names)))
-(def -mapping-name (:mapping (:business_menu_items cfg/elastic-search-names)))
+(def idx-name (:index (:business_menu_items cfg/elastic-search-names)))
+(def mapping-name (:mapping (:business_menu_items cfg/elastic-search-names)))
 
-(defn -item-search
+(defn- item-search
   "Perform search against item_id."
   [item-id geo-map page-map]
-  (es-doc/search -idx-name -mapping-name
+  (es-doc/search idx-name mapping-name
                  :query  {:match {:item_id item-id}}
                  ;; :sort   (array-map :yelp_star_rating  :desc
                  ;;                    :yelp_review_count :desc
@@ -78,6 +66,32 @@
                  :filter (util/mk-geo-filter geo-map)
                  :from   (:from page-map)
                  :size   (:size page-map)))
+
+
+(defn- get-prices
+  [{hits :hits}]
+  (let [prices (remove nil? (map #(-> % :_source :price_micros) (:hits hits)))
+        sum (apply + prices)
+        cnt (count prices)]
+    (if (= 0 cnt)
+      {:mean 0, :max 0, :min 0}
+      {:mean (/ sum cnt)
+       :max  (apply max prices)
+       :min  (apply min prices)})))
+    
+(defn- get-latest-close
+  "Get: minute, hour"
+  [{hits :hits}]
+  (let [day-of-week (util/get-day-of-week)
+        all-hours (remove nil? (map #(-> % :_source :business :hours) (:hits hits)))
+        all-hours-today (remove nil? (map #(util/get-hours-today % day-of-week) all-hours))
+        all-closing (map :close all-hours-today)
+        as-minutes (fn [time] (+ (:minute time) (* 60 (:hour time))))
+        max-mins (apply max (cons 0 (map as-minutes all-closing)))
+        minute (mod max-mins 60)
+        hours  (/ (- max-mins minute) 60)]
+    {:hour hours
+     :minute minute}))
 
 (defn validate-and-search
   ""
@@ -94,7 +108,23 @@
           
         ;; OK, do search.
         (let [page-map (inputs/mk-page-map input-page-map)
-              item-res (-item-search item-id geo-map page-map)]
+              all-page-map {:from 0, :size 100}
+              ;; Use all-page-map to make the ES query.
+              all-item-res (item-search item-id geo-map all-page-map)
+              ;; Gather metadata from the results returned.
+              prices       (get-prices all-item-res)
+              latest-close (get-latest-close all-item-res)
+
+              ;; THIS SURE IS UGLY, BUT IT WORKS.
+
+              ;; Then use page-map to restrict results.
+              hits (:hits all-item-res)
+              item-hits (:hits hits)
+              new-item-hits (take (:size page-map)
+                                  (drop (:from page-map) item-hits))
+              new-hits (assoc hits :hits new-item-hits)
+              item-res (assoc all-item-res :hits new-hits)]
             
             ;; Extract info from ES-results, create JSON response.
-            (-mk-response item-res item-id geo-map page-map))))))
+            (mk-response item-res prices latest-close
+                         item-id geo-map page-map))))))
