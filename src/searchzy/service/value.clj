432ffdaed145efdc8c795951230ceec87faf952c
Mark Wong-VanHaren <markwvh@gmail.com>
2013-11-21 03:14:04 +0100

    Further simplified sort-by-awesomeness logic.  Faster.

--- src/searchzy/service/value.clj
+++ src/searchzy/service/value.clj
@@ -10,57 +10,47 @@
 ;; Item:  {:price_micros, :yelp_star_rating, :yelp_review_count}
 
 (defn yelp-tweak
-  "Given a biz-menu-item, return one with additional 'tweaked-' attrs.
+  "Given a biz-menu-item, return one with additional attrs
+   :tweaked-count and :tweaked-rating.
    (If Yelp rating < 3.0, multiply number of reviews by -1.
     If no Yelp reviews, rating=3 and num-reviews=1.)"
   [item]
-  (let [r (:yelp_star_rating  (:_source item))
-        c (:yelp_review_count (:_source item))
-        rate-key  :tweaked-rating
-        count-key :tweaked-count]
+  (let [add (fn [i r c] (assoc i :tweaked-rating r :tweaked-count c))
+        r (:yelp_star_rating  (:_source item))
+        c (:yelp_review_count (:_source item))]
     (if (or (nil? c) (= c 0))
-      (assoc item rate-key 3.0 count-key 1)
+      (add item 3.0 1)
       (if (or (nil? r) (< r 3.0))
-        (assoc item rate-key (or r 0.0) count-key (* -1 c))
-        (assoc item rate-key (or r 0.0) count-key c)))))
+        (add item (or r 0.0) (* -1 c))
+        (add item (or r 0.0) c)))))
 
+(defn price-tweak
+  "Ensure that price isn't null.  Add :tweaked-price."
+  [item]
+  (assoc item :tweaked-price
+    (or (:price_micros item) Integer/MAX_VALUE)))
+        
 ;;--------
 
-(defn add-rank
-  "Add 'rank-attr' attribute to each item, based on its 'val-attr'.
-   'cmp' affects sort (i.e. either '<' [asc] or '>' [desc])."
-  [maps val-attr cmp rank-attr]
-  (let [[_ _ _ ranked]
-        (reduce (fn [[prev-r next-r prev-v res] m]
+(defn add-norm
+  "val-attr: #{:tweaked-count :tweaked-rating :price_micros}
+   cmp: affects sort.  < (asc), > (desc)
+   Adds norm (e.g. :tweaked-count-norm) to each item."
+  [maps val-attr cmp]
+  (let [norm-key (join-keys val-attr :norm)
+        max-rank (count maps)
+        add-norm (fn [m r] (assoc m norm-key (/ r max-rank)))
+        [_ _ _ normed]
+        (reduce (fn [[prev-r prev-v next-r res] m]
                   (let [val (get m val-attr)]
                     (if (= val prev-v)
-                      [prev-r (inc next-r) val
-                       (cons (assoc m rank-attr prev-r) res)]
-                      [next-r (inc next-r) val
-                       (cons (assoc m rank-attr next-r) res)])))
-                [0 1 nil ()]
+                      [prev-r val (inc next-r)
+                       (cons (add-norm m prev-r) res)]
+                      [next-r val (inc next-r)
+                       (cons (add-norm m next-r) res)])))
+                [0 nil 1 ()]
                 (sort-by val-attr cmp maps))]
-    (reverse ranked)))
-
-(defn add-norm-to-ranked-items
-  [ranked-items max-rank rank-key norm-key]
-  (let [norm (fn [i] (/ (get i rank-key) max-rank))]
-    (map #(assoc % norm-key (norm %)) ranked-items))
-
-(defn add-norm
-  "rank-attr: #{:tweaked-count :tweaked-rating :price_micros}
-   cmp: affects sort.  < (asc), > (desc)
-   Adds normed score (e.g. :tweaked-count-norm) to each item."
-  [items cmp rank-attr]
-  (let [rank-key     (join-keys rank-attr :rank)
-        norm-key     (join-keys rank-attr :norm)]
-    (-> items
-        (add-rank rank-attr cmp rank-key)
-        (add-norm-to-ranked-items (count items)
-                                  rank-key norm-key))))
-
-(defn first-gt [[a _] [b _]] (> a b))
-(defn first-lt [[a _] [b _]] (< a b))
+    (reverse normed)))
 
 ;; Rating/Reviews Factor
 ;; Rank Rating/Reviews independently by ascending order (i.e., lowest number ranked 1)
@@ -69,24 +59,23 @@
 (defn add-yelp-norms
   [items]
   (-> (map yelp-tweak items)
-      (add-norm first-lt :tweaked-count)
-      (add-norm first-lt :tweaked-rating)))
-        
+      (add-norm :tweaked-count <)
+      (add-norm :tweaked-rating <)))
+
 ;; Price Factor
 ;;   Rank Price by descending order (i.e., highest number ranked 1)
 ;;   Take ranking for Price and divide by max rank for that bucket
 ;;   Apply weight to score
 (defn add-price-norm
   [items]
-  (-> items
-      (add-norm first-gt :price_micros)))
+  (-> (map price-tweak items)
+      (add-norm :tweaked-price >)))
 
 (defn add-score-to-item
   [item]
-  (let [score (+
-               (* 0.4 (:price_micros-norm   item))
-               (* 0.3 (:tweaked-rating-norm item))
-               (* 0.3 (:tweaked-count-norm  item)))]
+  (let [score (+ (* 0.4 (:tweaked-price-norm  item))
+                 (* 0.3 (:tweaked-rating-norm item))
+                 (* 0.3 (:tweaked-count-norm  item)))]
     (assoc item :awesomeness score)))
 
 (defn add-score
@@ -94,6 +83,7 @@
   (map add-score-to-item items))
 
 (defn score-and-count-gt
+  "comparator for sorting"
   [i1 i2]
   (let [a1 (:awesomeness i1)
         a2 (:awesomeness i2)
